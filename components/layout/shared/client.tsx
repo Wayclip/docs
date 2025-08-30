7e698f7574b20e72efe1ee57ddaf0cf7053a3e8b
konyogony <me@konyogony.dev>
2025-08-30 22:38:48 +0700

    Fix workflow & format files

--- components/layout/shared/client.tsx
+++ components/layout/shared/client.tsx
@@ -5,26 +5,14 @@ import { isActive } from '../../../lib/is-active';
 import Link from 'fumadocs-core/link';
 import type { BaseLinkType } from './index';
 
-export function BaseLinkItem({
-  ref,
-  item,
-  ...props
-}: Omit<ComponentProps<'a'>, 'href'> & { item: BaseLinkType }) {
-  const pathname = usePathname();
-  const activeType = item.active ?? 'url';
-  const active =
-    activeType !== 'none' &&
-    isActive(item.url, pathname, activeType === 'nested-url');
+export function BaseLinkItem({ ref, item, ...props }: Omit<ComponentProps<'a'>, 'href'> & { item: BaseLinkType }) {
+    const pathname = usePathname();
+    const activeType = item.active ?? 'url';
+    const active = activeType !== 'none' && isActive(item.url, pathname, activeType === 'nested-url');
 
-  return (
-    <Link
-      ref={ref}
-      href={item.url}
-      external={item.external}
-      {...props}
-      data-active={active}
-    >
-      {props.children}
-    </Link>
-  );
+    return (
+        <Link ref={ref} href={item.url} external={item.external} {...props} data-active={active}>
+            {props.children}
+        </Link>
+    );
 }
